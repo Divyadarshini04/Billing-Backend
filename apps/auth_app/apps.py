d96aa1e86c5018e574e1cc3b0d40519a2c8668de
Divyadarshini04 <134588703+Divyadarshini04@users.noreply.github.com>
2026-01-20 08:25:19 +0530

    Auto create super admin on startup

--- apps/auth_app/apps.py
+++ apps/auth_app/apps.py
@@ -1,8 +1,22 @@
 from django.apps import AppConfig
 
 class AuthAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.auth_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.auth_app"
 
     def ready(self):
-        import apps.auth_app.signals
+        try:
+            from apps.auth_app.models import User
+
+            if not User.objects.filter(is_super_admin=True).exists():
+                user = User.objects.create(
+                    phone="9342547471",
+                    is_super_admin=True,
+                    is_staff=True,
+                    is_active=True
+                )
+                user.set_password("Admin@123")
+                user.save()
+                print("✅ Super Admin auto-created")
+        except Exception as e:
+            print("⚠️ Super Admin auto-create skipped:", e)
